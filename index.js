72d1b47f6d0dd07ab5cb8186a32bc62176b5193e
Matt Tarasoff <tarasoff.matt@gmail.com>
2025-01-28 18:11:07 -0500

    Added getUsers to routes and tested working

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const db = require('./queries');
 const PORT = 3000;
 
 app.use(bodyParser.json());
-
 app.use(
     bodyParser.urlencoded({
         extended: true
@@ -14,6 +14,8 @@ app.use(
 app.get('/', (request, response) => {
     response.json({ info: 'Node.js, Express, and Postgres API' })
   });
+
+app.get('/users', db.getUsers);
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}.`)
---
+++ queries.js
@@ -0,0 +1,26 @@
+// load env variables
+require('dotenv').config();
+
+
+// create database connection pool
+const Pool = require('pg').Pool;
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
+});
+
+const getUsers = (request, response) => {
+    pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+        if (error) {
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    });
+};
+
+module.exports = {
+    getUsers
+};
